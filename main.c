c53a131dd355a68dc88064320e3e9299ab16ffb1
Zhanetta11 <150706815+Zhanetta11@users.noreply.github.com>
2024-09-22 14:03:18 +0600

    Make Inputs

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double myNum;
+    printf("Enter a decimal number: ");
+    scanf("%lf", &myNum);
+    
+    int nk;
+    printf("Enter nk: ");
+    scanf("%d", &nk);
+
+    return 0;
+}
